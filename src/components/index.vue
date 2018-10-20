71b27194c316d81c227efbacab0b1f3cb68c1eb9
bobo1 <18671457993@139.com>
2018-10-20 08:03:48 +0800

    1020

--- src/components/index.vue
+++ src/components/index.vue
@@ -21,12 +21,12 @@
   <!-- 广告 -->
   <div class="home-internet-hospital-top">
     <div class="home-internet-hospital-top-entry">
-      <router-link to="">
+      <router-link to="" class="top-entry">
         <img src="../../public/img/index/pic-internet-hospital.fe75ca6398fa9c09353c776104189dd9.png" alt="">
       </router-link>
     <!-- 挂号,医生 -->
       <section class="home-internet-hospital-entries--item">
-        <router-link to="" v-for="tmp of entry" :key="tmp.id">
+        <router-link to="" v-for="tmp of entry" :key="tmp.id" class="entries--item">
           <p>
             <img :src="tmp.img" alt="">
             <span class="home-internet-hospital-entries--item-title">{{tmp.title}}</span>
@@ -38,24 +38,38 @@
       </section>
       <hr>
       <!-- 特色专科 -->
-      <div class="home-internet-hospital-departments--header">
+      <div class="departments--header">
         <h3>特色专科</h3>
         <router-link to="">全部科室 ></router-link>
       </div>
-      <div class="home-internet-hospital-departments--content">
-      <div class="home-internet-hospital-departments--item">
-        <router-link to="" v-for="department of departments" :key="department.id" class="home-internet-hospital-departments-item">
-          <img :src="department.img" alt="">
-          <p>{{department.title}}</p>
-          <p>{{department.doctor}}</p>
-        </router-link>
+      <div class="departments--content">
+        <div class="departments--item">
+          <router-link to="" v-for="department of departments" :key="department.index">
+            <img :src="department.img" alt="">
+            <p>{{department.title}}</p>
+            <p>{{department.doctor}}</p>
+          </router-link>
+        </div>
       </div>
+      <hr>
+      <!-- 微医全科-->
+      <div class="home-internet-hospital--qk">
+      <router-link to="">
+          <img src="../../public/img/index/pic-qk.28675500fe03acc028c18114fa4eb676.png" alt="">
+      </router-link>
+      <hr>
       </div>
+      <!-- 就诊动态 -->
+        <section class="hospital-dynamics">
+          <div class="hospital-dynamics--header">
+          <h3>就诊动态</h3>
+          <router-link to="">去看病</router-link>
+          </div>
+        </section>
     </div>
   </div>
   </div>
 </template>
-
 <script>
 export default {
   data(){
@@ -91,11 +105,12 @@ export default {
   
   div.home-internet-hospital-top-entry{
       width: 90%;
+      height: 700px;
       box-shadow:0px 0px 10px 10px #F6F6F7;
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
   }
-  img{
+  div.home-internet-hospital-top-entry a.top-entry img{
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
   }
@@ -107,7 +122,7 @@ export default {
   }
   section.home-internet-hospital-entries--item a{
       display: block;
-      width: 45%;
+      width: 40%;
   }
   section.home-internet-hospital-entries--item a img{
     width: 27px;
@@ -115,35 +130,70 @@ export default {
     border-top-left-radius: 0px;
     border-top-right-radius: 0px;
   }
+  p{
+    margin: 0px !important;
+  }
   /* 特色专科 */
-  div.home-internet-hospital-departments--header{
+  div.departments--header{
     display: flex;
     justify-content: space-between;
   }
-  div.home-internet-hospital-departments--header h3{
+  div.departments--header h3{
     font-weight:700px;
     font-size: 20px;
     margin-left: 13px;
   }
-  div.home-internet-hospital-departments--header a{
+  div.departments--header a{
     margin-top:5px;
     margin-right: 13px;
     color: #83889A;
     font-size: 14px;
   }
   /* 特色专科--目录 */
-  div.home-internet-hospital-departments--content{
+  div.departments--item{
     display: flex;
     justify-content: center;
     width: 90%;
   }
-  div.home-internet-hospital-departments--item{
+  div.departments--content{
     display: flex;
-    justify-content: space-between;
-    width: 90%;
+    justify-content: center;
   }
-  div.home-internet-hospital-departments--item img{
+  div.departments--item a.router-link-exact-active{
+    display: block;
     width: 25%;
-    
+  }
+  div.departments--item{
+    flex-wrap: wrap;
+  }
+  div.departments--item a.router-link-exact-active img{
+    position: relative;
+    width:35px;
+    height:35px;
+    left:10px;
+    top:5px;
+  }
+  div.departments--item a.router-link-exact-active p{
+    margin: 0px;
+    font-size: 10px;
+    color:#5F697A;
+  }
+  div.departments--item a.router-link-exact-active{
+    background: #F6F7F8;
+    border: 5px solid #FFFFFF;
+    border-radius: 10px;
+  }
+  /* 微医全科 */
+  div.home-internet-hospital--qk a img{
+    width: 90%;
+    height: 80px; 
+  }
+  div.home-internet-hospital--qk a{
+    display: flex;
+    justify-content: center;
+    border-radius: 10px;
+  }
+  hr{
+    width: 90%;
   }
 </style>
