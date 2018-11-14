a8d0572413149c5a3e1d34e78c2be612b43eb652
bobo1 <18671457993@139.com>
2018-11-14 22:25:12 +0800

    1114

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,28 +1,48 @@
 <template>
   <div>
-    <div class="product-header">
-      <router-link to="/home/homes"><img src="../../public/img/personage/back.png" alt=""></router-link>
-      <h3>购物车(2)</h3>
-    </div>
-    <!-- product-top -->
-    <div class="product-top">
-      <ul>
-        <li>
-          <div class="product-left"><img src="../../public/img/product/check.png" alt=""></div>
-          <div class="product-right">
-            <img src="https://kano.guahao.cn/kn447624855" alt="">
-            <div class="product-right-span">
-              <span>【双11疯抢，买2包即送6包黑枸杞】谷生园 乌圣糕</span>
-              <p></p>
-            </div>
-          </div>
-        </li>
-        <li></li>
-      </ul>
-    </div>
+   <div class="product-preview-imges">
+     <ul>
+       <li><img src="../../public/img/product/52987_O1.jpg" alt=""></li>
+     </ul>
+   </div>
+   <div class="product-info">
+     <p class="product-name">康美（山药） 150g/瓶</p>
+     <div class="product-price">
+       <span class="product-price-one">32元</span>
+       <span class="product-price-market">98元</span>
+     </div>
+   </div>
+   <div class="div-hr"></div>
+   <div class="product-intr">
+     <p class="product-title">保健品说明书</p>
+     <ul>
+       <li><label for="">功能主治:</label><p>补脾养胃，生津益肺，用于脾虚食少</p></li>
+       <li><label for="">规&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;格:</label><p>150g/瓶</p></li>
+       <li><label for="">用法用量:</label><p>15~30g</p></li>
+       <li><label for="">有&nbsp;&nbsp;效&nbsp;&nbsp;期:</label><p>36个月</p></li>
+       <li><label for="">执行标准:</label><p>《中国药典》2015年版</p></li>
+       <li><label for="">批文准字:</label><p>1</p></li>
+     </ul>
+   <div class="div-hr"></div>
+   </div>
+   <div class="product-footer">
+     <ul>
+       <li>
+        <router-link to="">
+           <img src="../../public/img/product/car.png" alt="">
+        </router-link>
+         <span class="product-car">0</span>
+       </li>
+       <li>
+         <router-link to="">加入清单</router-link>
+       </li>
+       <li>
+         <router-link to="">立即购买</router-link>
+       </li>
+     </ul>
+   </div>
   </div>
 </template>
-
 <script>
 export default {
 
@@ -30,51 +50,95 @@ export default {
 </script>
 
 <style scoped>
-  div.product-header {
-  text-align: center;
-  margin-top: -15px;
-  position: fixed;
-  width: 100%;
-  height: 40px;
-  line-height: 40px;
-  background: #ffffff;
-  z-index: 1;
-}
-div.product-header img{
-  height: 20px;
-  width: 20px;
-  margin-top: 10px;
-  margin-left: 13px;
-}
-div.product-header h3 {
-  font-weight: 500;
-}
-div.product-header a {
-  position: absolute;
-  left: 0px;
-}
-/* product-top */
-div.product-top{
-  position: relative;
-  top:40px;
-  width: 90%;
-  margin: 0 auto;
-}
-div.product-top ul{
-  list-style: none;
-  display: block;
-  padding: 0px;
-}
-div.product-top ul li div.product-right{
-  width: 22px;
-  height: 22px;
-}
-div.product-right{
-  display: flex;
-  justify-content: space-between;
-}
-div.product-right img{
-  height: 100px;
-  width: 100px;
-}
+ div.product-preview-imges ul{
+   list-style: none;
+   padding: 0px;
+   margin: 0px;
+ }
+ div.product-preview-imges{
+   border-bottom: 1px solid #EEEEEE;
+ }
+ div.product-preview-imges ul li img{
+   width: 100%;
+ }
+ div.product-info{
+   width: 90%;
+   margin: 0 auto;
+ }
+ div.product-intr p{
+   text-align: center;
+   height: 40px;
+   line-height: 40px;
+ }
+ div.product-intr{
+   border-bottom: 1px solid  #EEEEEE;
+ }
+ div.product-intr ul{
+   list-style:none;
+   padding: 0px;
+ }
+ div.product-intr ul li{
+   font-size: 12px;
+   display: flex;
+ }
+ div.product-intr ul li label{
+   line-height: 40px;
+   width: 60px;
+   text-align: center;
+ }
+ div.product-info{
+   font-size: 14px;
+ }
+ div.product-price{
+   font-size: 24px;
+ }
+ div.product-price span:first-child{
+   color: #FF695C;
+ }
+ div.product-price span:last-child{
+   font-size: 14px;
+ }
+ div.product-intr p.product-title{
+   font-size: 14px;
+ }
+ div.product-footer{
+   position:fixed;
+   bottom: 0px;
+   width: 100%;
+   height: 50px;
+   background: #FFFFFF;
+   border-top:1px solid #EEEEEE;
+   margin-top:10px;
+   line-height: 50px;
+ }
+ div.product-footer ul{
+    list-style: none;  
+ }
+ div.product-footer ul{
+   display: flex;
+   justify-content: space-around;
+   padding: 0px;
+   margin: 0px;
+ }
+ div.product-footer ul li a img{
+   height:30px;
+   width:30px;
+   margin-top:10px;
+ }
+ div.product-footer span.product-car{
+   position: absolute;
+   height: 16px;
+   width: 16px;
+   left:60px;
+   top:5px;
+   line-height: 16px;
+   text-align: center;
+   border-radius: 50%;
+   font-size: 12px;
+   color:#FFFFFF;
+   background: #FF3C00;
+ }
+ div.product-footer ul li{
+   border:1px solid #EEEEEE;
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,13 +8,11 @@ Vue.config.productionTip = false
 Vue.prototype.$axios = Axios;
 import "mint-ui/lib/style.css"
 import { Swipe, SwipeItem} from 'mint-ui'
-import {Button} from 'mint-ui'
 // 引入toast组件
 import{Toast} from 'mint-ui'
 Vue.component(Swipe.name,Swipe)
 Vue.component(SwipeItem.name,SwipeItem)
 Vue.prototype.$toast = Toast;
-Vue.component(Button.name,Button)
 
 
 new Vue({
